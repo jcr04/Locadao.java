0f98af6a4d1f7dee13f603bcf2a606accad0f0dd
jcr04 <joaocarlosreboucas15@gmail.com>
2023-11-02 18:12:32 -0300

    add api

---
+++ src/main/java/com/projetos/Locadao/LocadaoApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.Locadao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos/Locadao/LocadaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.Locadao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
